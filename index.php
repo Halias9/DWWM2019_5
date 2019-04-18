a1ba8275988c6aed63e3bc02dee10f3472e78c70
halias9 <lisonhans@gmail.com>
2019-04-18 19:01:08 -0400

    new file:   CSS/style.css 	new file:   images/gps-home-.png 	new file:   index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Projet Gwada Bat</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Black+Ops+One" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+ <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+   
+    <h2>GWADA BAT</h2>
+    <!-- Image and text -->
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="images/gps-home-.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    Home
+  </a>
+  <ul class="g">
+      <li><a href="#">Articles</a></li>  
+      <li><a href="#">Commandes</a></li>  
+      <li><a href="#">Clients</a></li>
+  </ul>
+</nav>
+<br>
+
+<form>
+   NOM <input type="text" for="name">
+   <br> <br>
+  Quantité <select name="Quantité" size="1">
+  <option>1</option>
+  <option>2</option>
+  <option>3</option>
+  <option>4</option>
+  <option>5</option>
+  <option>6</option>
+  <option>7</option>
+  </select>
+
+Unité <select name="unité" size="1">
+    <option>Kg</option>
+    <option>m3</option>
+    <option>L</option>
+</select>
+</form>
+
+
+
+ <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+
+</html>
